43ee76a37a194d773297636e1a0c28d28c57eb92
Irara <irara.chen@token.im>
2024-10-15 15:34:54 +0800

    feat: Event-driven action script

--- actions/offChainPaymasterAction.ts
+++ actions/offChainPaymasterAction.ts
@@ -37,6 +37,16 @@ const entryPointAbi = [
 const postOpRevertReasonId =
   "0xf62676f440ff169a3a9afdbf812e89e7f95975ee8e5c31214ffdef631c5f4792";
 
+// Identifier for UserOperationEvent event
+// = keccak256(abi.encodePacked("UserOperationEvent(bytes32,address,address,uint256,bool,uint256,uint256)"))
+const userOperationEventId =
+  "0x49628fd1471006c1482da88028e9ce4dbb080b815c9b0344d39e5a8e6ec1419f";
+
+// Identifier for UserOpProcessed event
+// = keccak256(abi.encodePacked("UserOpProcessed(bytes32,address,bytes32,uint8,uint256,address,uint256,address,bool)"))
+const userOpProcessedId =
+  "0x4a7d89094dad8258a8c7f96c6cad9b077fe57305ac3e2da96478295d1b48c7d9";
+
 // Paymaster operation modes
 enum PaymasterMode {
   Sponsor,
@@ -67,12 +77,36 @@ interface PaymasterDataStruct {
   maxCostAllowed: bigint;
 }
 
+// Structure for UserOperationEvent event
+export interface UserOpEventParams {
+  userOpHash: string;
+  sender: string;
+  paymaster: string;
+  nonce: bigint;
+  success: boolean;
+  actualGasCost: bigint;
+  actualGasUsed: bigint;
+}
+
 // Structure for PostOpRevertReason event
-interface PostOpRevertReasonEvent {
+export interface PostOpRevertReasonEventParams {
   userOpHash: string;
   sender: string;
   nonce: bigint;
   revertReason: string;
+}
+
+// Structure for UserOpProcessed event
+export interface UserOpProcessedEventParams {
+  userOpHash: string;
+  userOpSender: string;
+  signerDataHash: string;
+  mode: PaymasterMode;
+  actualGasCost: bigint;
+  token: string;
+  actualTokenCost: bigint;
+  chargeFrom: string;
+  chargeSuccessful: boolean;
 }
 
 // Logs JSON data, converting BigInt to string
@@ -80,19 +114,19 @@ const jsonStringify = (data: any): string => {
   return JSON.stringify(
     data,
     (_, value) => {
-      return typeof value === "bigint" ? value.toString() : value;
+      return typeof value === "bigint" ? `0x${value.toString(16)}` : value;
     },
     2
   );
 };
 
 // Prints JSON data to console with a title
-const printJson = (title: string, data: Object) => {
+export const printJson = (title: string, data: Object) => {
   console.log(`${title}: ${jsonStringify(data)}`);
 };
 
 // Parses handleOps function input from decoded data
-const parseHandleOpsInput = (
+const decodeHandleOpsInput = (
   input: Result
 ): {
   ops: PackedUserOperationInput[];
@@ -206,43 +240,208 @@ const decodePaymasterAndDataStruct = (
   }
 };
 
-// Decodes the PostOpRevertReason event from logs
-const decodePostOpRevertReasonEvent = (
-  logs: Log[]
-): PostOpRevertReasonEvent | null => {
-  const eventLog = logs.find((log) => log.topics[0] === postOpRevertReasonId);
-
-  // Log warning if event not found
-  if (!eventLog) {
-    console.warn(`PostOpRevertReason event not found`);
-    return null;
-  }
-
-  // Extract userOpHash from indexed topic
-  const userOpHash = eventLog.topics[1];
-
-  // Extract sender address from indexed topic
-  const sender = getAddress("0x" + eventLog.topics[2].slice(26));
-
-  // Extract nonce and revertReason from data
-  const [nonce, revertReason] = AbiCoder.defaultAbiCoder().decode(
-    ["uint256", "bytes"],
-    eventLog.data
+// Decodes the UserOperationEvent event from logs
+const parseUserOpEvent = (params: {
+  logs: Log[];
+  filterUserOpHashes?: string[];
+  filterPaymasters?: string[];
+}): UserOpEventParams[] => {
+  const eventLogs = params.logs.filter(
+    (log) => log.topics[0] === userOperationEventId
   );
 
-  return {
-    userOpHash,
-    sender,
-    nonce,
-    revertReason,
-  };
+  // Log warning if no matching events are found
+  if (eventLogs.length === 0) {
+    console.warn(`UserOperationEvent event not found`);
+    return [];
+  }
+
+  // Use reduce to accumulate valid events
+  const decodedUserOpEvents = eventLogs.reduce<UserOpEventParams[]>(
+    (userOps, eventLog) => {
+      // Extract userOpHash from indexed topic
+      const userOpHash = eventLog.topics[1];
+
+      // Skip if the userOpHash doesn't match the filter (if provided)
+      if (
+        params.filterUserOpHashes &&
+        !params.filterUserOpHashes.includes(userOpHash)
+      ) {
+        return userOps;
+      }
+
+      // Extract sender and paymaster address from indexed topic
+      const sender = getAddress("0x" + eventLog.topics[2].slice(26));
+
+      const paymaster = getAddress("0x" + eventLog.topics[3].slice(26));
+
+      // Skip if the paymaster doesn't match the filter (if provided)
+      if (
+        params.filterPaymasters &&
+        !params.filterPaymasters.includes(paymaster)
+      ) {
+        return userOps;
+      }
+
+      // Extract nonce, success, actualGasCost and actualGasUsed from data
+      const [nonce, success, actualGasCost, actualGasUsed] =
+        AbiCoder.defaultAbiCoder().decode(
+          ["uint256", "bool", "uint256", "uint256"],
+          eventLog.data
+        );
+
+      userOps.push({
+        userOpHash,
+        sender,
+        paymaster,
+        nonce,
+        success,
+        actualGasCost,
+        actualGasUsed,
+      });
+
+      return userOps;
+    },
+    []
+  );
+
+  if (decodedUserOpEvents.length === 0) {
+    console.warn(`No UserOperationEvent events matched the provided filter`);
+  }
+
+  return decodedUserOpEvents;
+};
+
+// Decodes the PostOpRevertReason event from logs
+const parsePostOpRevertReasonEvents = (params: {
+  logs: Log[];
+  filterUserOpHashes?: string[];
+}): PostOpRevertReasonEventParams[] => {
+  const eventLogs = params.logs.filter(
+    (log) => log.topics[0] === postOpRevertReasonId
+  );
+
+  // Log warning if event not found
+  if (eventLogs.length === 0) {
+    console.warn(`PostOpRevertReason event not found`);
+    return [];
+  }
+
+  const decodedPostOpRevertReasonEvents = eventLogs.reduce<
+    PostOpRevertReasonEventParams[]
+  >((postOpRevertReasons, eventLog) => {
+    // Extract userOpHash from indexed topic
+    const userOpHash = eventLog.topics[1];
+
+    // Skip if the userOpHash doesn't match the filter (if provided)
+    if (
+      params.filterUserOpHashes &&
+      !params.filterUserOpHashes.includes(userOpHash)
+    ) {
+      return postOpRevertReasons;
+    }
+
+    // Extract sender address from indexed topic
+    const sender = getAddress("0x" + eventLog.topics[2].slice(26));
+
+    // Extract nonce and revertReason from data
+    const [nonce, revertReason] = AbiCoder.defaultAbiCoder().decode(
+      ["uint256", "bytes"],
+      eventLog.data
+    );
+
+    postOpRevertReasons.push({
+      userOpHash,
+      sender,
+      nonce,
+      revertReason,
+    });
+
+    return postOpRevertReasons;
+  }, []);
+
+  if (decodedPostOpRevertReasonEvents.length === 0) {
+    console.warn(`No PostOpRevertReason events matched the provided filter`);
+  }
+
+  return decodedPostOpRevertReasonEvents;
+};
+
+// Decodes the UserOpProcessed event from logs
+const parseUserOpProcessedEvents = (params: {
+  logs: Log[];
+  filterUserOpHashes?: string[];
+}): UserOpProcessedEventParams[] => {
+  const eventLogs = params.logs.filter(
+    (log) => log.topics[0] === userOpProcessedId
+  );
+
+  // Log warning if event not found
+  if (eventLogs.length === 0) {
+    console.warn(`UserOpProcessed event not found`);
+    return [];
+  }
+
+  const decodedUserOpProcessedEvents = eventLogs.reduce<
+    UserOpProcessedEventParams[]
+  >((UserOpProcesseds, eventLog) => {
+    // Extract userOpHash from indexed topic
+    const userOpHash = eventLog.topics[1];
+
+    // Skip if the userOpHash doesn't match the filter (if provided)
+    if (
+      params.filterUserOpHashes &&
+      !params.filterUserOpHashes.includes(userOpHash)
+    ) {
+      return UserOpProcesseds;
+    }
+
+    // Extract userOpSender address from indexed topic
+    const userOpSender = getAddress("0x" + eventLog.topics[2].slice(26));
+
+    const signerDataHash = eventLog.topics[3];
+
+    // Extract mode, actualGasCost, token and etc. from data
+    const [
+      mode,
+      actualGasCost,
+      token,
+      actualTokenCost,
+      chargeFrom,
+      chargeSuccessful,
+    ] = AbiCoder.defaultAbiCoder().decode(
+      ["uint8", "uint256", "address", "uint256", "address", "bool"],
+      eventLog.data
+    );
+
+    UserOpProcesseds.push({
+      userOpHash,
+      userOpSender,
+      signerDataHash,
+      mode: Number(mode),
+      actualGasCost,
+      token,
+      actualTokenCost,
+      chargeFrom,
+      chargeSuccessful,
+    });
+
+    return UserOpProcesseds;
+  }, []);
+
+  if (decodedUserOpProcessedEvents.length === 0) {
+    console.warn(`No UserOpProcessed events matched the provided filter`);
+  }
+
+  return decodedUserOpProcessedEvents;
 };
 
 // Sends notifications to Discord webhook
-const notifyDiscord = async (text: string, context: Context) => {
-  const webhookLink = await context.secrets.get(
-    "DISCORD_PAYMASTER_CHANNEL_WEBHOOK"
-  );
+const notifyDiscord = async (text: string, webhookLink: string) => {
+  if (!webhookLink) {
+    console.error(`Cannot find discord webhook link`);
+    return;
+  }
 
   const discordText = `🐥 ${text}`;
 
@@ -268,11 +467,17 @@ const notifyDiscord = async (text: string, context: Context) => {
   }
 };
 
+const pushToStorage = async (context: Context, key: string, value: any) => {
+  let jsonData = await context.storage.getJson(key);
+  if (jsonData && Object.keys(jsonData).length === 0) {
+    jsonData = [];
+  }
+  jsonData.push(JSON.parse(jsonStringify(value)));
+  await context.storage.putJson(key, jsonData);
+};
+
 // Do not change function name.
 export const actionFn: ActionFn = async (context: Context, event: Event) => {
-  //   console.log(`context: ${JSON.stringify(context, null, 2)}`);
-  //   console.log(`event: ${JSON.stringify(event, null, 2)}`);
-
   // To access project's secret
   // let secret = await context.secrets.get('MY-SECRET')
 
@@ -281,6 +486,41 @@ export const actionFn: ActionFn = async (context: Context, event: Event) => {
   // await context.storage.putStr('MY-KEY', 'MY-VALUE')
 
   // Your logic goes here :)
+  //
+
+  // Set storage at: https://dashboard.tenderly.co/IraraChen/monitoring/actions/storage
+  // Example:
+  //   MONITORED_PAYMASTER_ADDRESS = [
+  //     "0x44D6f8362c144A1217f24A11bE35f2c418B6cb20",
+  //     "0x81d7a78C455730d0cdEcD5123793C9596ABBf53a",
+  //   ];
+  const monitoredPaymasterAddress: string[] = await context.storage.getJson(
+    "MONITORED_PAYMASTER_ADDRESS"
+  );
+
+  console.log(
+    `monitoredPaymasterAddress: ${JSON.stringify(
+      monitoredPaymasterAddress,
+      null,
+      2
+    )}`
+  );
+
+  if (
+    monitoredPaymasterAddress &&
+    Object.keys(monitoredPaymasterAddress).length === 0
+  ) {
+    console.error(`Cannot get monitored paymaster address`);
+    return;
+  }
+
+  // Set secret at: https://dashboard.tenderly.co/IraraChen/monitoring/actions/secrets
+  // Example:
+  // DISCORD_PAYMASTER_CHANNEL_WEBHOOK=https://discord.com/api/webhooks/xxx/xxx
+  const discordWebhookLink = await context.secrets.get(
+    "DISCORD_PAYMASTER_CHANNEL_WEBHOOK"
+  );
+  console.log(discordWebhookLink);
 
   // Cast event to TransactionEvent type
   const transactionEvent = event as TransactionEvent;
@@ -292,6 +532,46 @@ export const actionFn: ActionFn = async (context: Context, event: Event) => {
   const input = transactionEvent.input;
   const logs = transactionEvent.logs as Log[];
 
+  // If paymaster is OffChainPaymaster, fetch UserOperationEvent event
+  const userOpEventLogs = parseUserOpEvent({
+    logs,
+    filterPaymasters: monitoredPaymasterAddress,
+  });
+  if (userOpEventLogs.length === 0) {
+    return;
+  }
+
+  const userOpHashes = userOpEventLogs.map((userOp) => userOp.userOpHash);
+
+  const userOpProcessedLogs = parseUserOpProcessedEvents({
+    logs,
+    filterUserOpHashes: userOpHashes,
+  });
+  if (userOpProcessedLogs.length === 0) {
+    return;
+  }
+
+  printJson("userOpProcessedLogs", userOpProcessedLogs);
+
+  for (const userOpProcessedLog of userOpProcessedLogs) {
+    if (userOpProcessedLog.mode !== PaymasterMode.ChargeInPostOp) {
+      continue;
+    }
+
+    if (userOpProcessedLog.chargeSuccessful) {
+      await pushToStorage(context, "ChargeInPostOpSuccess", userOpProcessedLog);
+    }
+
+    if (!userOpProcessedLog.chargeSuccessful) {
+      await pushToStorage(context, "ChargeInPostOpFail", userOpProcessedLog);
+      // Notify Discord with the post-operation revert reason
+      await notifyDiscord(
+        jsonStringify(userOpProcessedLog),
+        discordWebhookLink
+      );
+    }
+  }
+
   const entryPointIface = new Interface(entryPointAbi);
 
   // Decode input data for handleOps function
@@ -299,23 +579,20 @@ export const actionFn: ActionFn = async (context: Context, event: Event) => {
   //   printJson("decodedInput", decodedInput);
 
   // Parse user operations and beneficiary
-  const { ops } = parseHandleOpsInput(decodedInput);
+  const { ops } = decodeHandleOpsInput(decodedInput);
   //   printJson("ops", ops);
 
   // Process each operation
   for (const op of ops) {
-    // console.log(`paymasterAndData: ${op.paymasterAndData}`);
-
     // Decode paymasterAndData into structured fields
     const decodedPaymasterAndData = decodePaymasterAndDataStruct(
       op.paymasterAndData
     );
 
+    // Invalid paymasterAndData encountered
     if (!decodedPaymasterAndData) {
-      console.warn(`Invalid paymasterAndData encountered`);
       continue;
     }
-    printJson("Decoded Paymaster Data", decodedPaymasterAndData);
 
     if (decodedPaymasterAndData.mode !== PaymasterMode.ChargeInPostOp) {
       console.warn(`Paymaster mode is not ChargeInPostOp`);
@@ -323,16 +600,12 @@ export const actionFn: ActionFn = async (context: Context, event: Event) => {
     }
 
     // If paymaster mode is ChargeInPostOp, fetch PostOpRevertReason event
-    const postOpRevertReasonLog = decodePostOpRevertReasonEvent(logs);
+    const postOpRevertReasonLog = parsePostOpRevertReasonEvents({ logs });
 
+    // PostOpRevertReason event not found
     if (!postOpRevertReasonLog) {
-      console.warn(`PostOpRevertReason event not found`);
       continue;
     }
-    printJson("postOpRevertReasonLog", postOpRevertReasonLog);
-
-    // Notify Discord with the post-operation revert reason
-    await notifyDiscord(jsonStringify(postOpRevertReasonLog), context);
   }
 
   console.log(`Tenderly Web3 Action script completed`);
